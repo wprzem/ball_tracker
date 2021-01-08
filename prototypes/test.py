4a7edb6f3b7719b09b7a00dd44b9ef9420d897f2
pw <wozniak.przem@gmail.com>
2021-01-08 18:45:24 +0100

    python prototype - load video

---
+++ prototypes/test.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('../images/movie.avi')
+cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grey = cv2.GaussianBlur(grey, (5, 5), 0)
+    circles = cv2.HoughCircles(
+        image=grey,
+        method=cv2.HOUGH_GRADIENT,
+        dp=1.5,
+        minDist=800,
+        param1=300,
+        param2=0.9,
+        minRadius=200,
+        maxRadius=250)
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            # draw the outer circle
+            cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
