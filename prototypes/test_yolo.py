7589a4855440a9872fa9251c9d3cc95ba7152091
LeszczM <leszczynskimaciej@10g.pl>
2021-03-14 16:23:46 +0100

    adding weights, changes in coco.names, test_yolo, yolov3cfg

--- prototypes/test_yolo.py
+++ prototypes/test_yolo.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+net = cv2.dnn.readNet("yolov3_training_last.weights", "yolov3.cfg")
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
@@ -10,13 +10,14 @@ with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 
 # cap = cv2.VideoCapture('../images/movie.avi')
-cap = cv2.VideoCapture('../images/2020_1116_191622_082.MOV')
+cap = cv2.VideoCapture('../images/2020_1114_132258_076.mov')
 
 while(cap.isOpened()):
     ret, img = cap.read()
     if img is not None:
-        # img = cv2.resize(img, None, fx=0.4, fy=0.4)
-        
+        img = cv2.resize(img, None, fx=0.4, fy=0.4)
+        #img = cv2.flip(img, 0)
+        img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
         height, width, channels = img.shape
 
         blob = cv2.dnn.blobFromImage(img, 1 / 255, (416, 416), (0, 0, 0), True, crop=False)
@@ -31,7 +32,7 @@ while(cap.isOpened()):
                 scores = detection[5:]
                 class_id = np.argmax(scores)
                 confidence = scores[class_id]
-                if confidence > 0.3:
+                if confidence > 0.5:
                     # Object detected
                     center_x = int(detection[0] * width)
                     center_y = int(detection[1] * height)
